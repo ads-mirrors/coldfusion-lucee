8fd5ef336275182e015e77cdad486682d594e2cb
michaeloffner <michaeloffner@users.noreply.github.com>
2024-12-21 16:54:11 +0100

    check if directory exists

--- core/src/main/java/lucee/runtime/config/ConfigWebFactory.java
+++ core/src/main/java/lucee/runtime/config/ConfigWebFactory.java
@@ -1738,7 +1738,7 @@ public final class ConfigWebFactory extends ConfigFactory {
 
 				try {
 					// first we delete the physical classes
-					config.getClassDirectory().remove(true);
+					if (config.getClassDirectory().exists()) config.getClassDirectory().remove(true);
 
 					// now we force the pagepools to flush
 					flushPageSourcePool(config.getMappings());
