813eed73f54bbf696b4038b9b10d1b47d15482b9
michaeloffner <michaeloffner@users.noreply.github.com>
2025-02-28 21:02:55 +0100

    LDEV-5355 -  Replace HashMap with ConcurrentHashMap  to prevent ConcurrentModificationException

--- core/src/main/java/lucee/runtime/listener/ApplicationContextSupport.java
+++ core/src/main/java/lucee/runtime/listener/ApplicationContextSupport.java
@@ -18,7 +18,6 @@
  */
 package lucee.runtime.listener;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -103,7 +102,7 @@ public abstract class ApplicationContextSupport implements ApplicationContext {
 		applicationtoken = other.applicationtoken;
 
 		if (other.tagDefaultAttributeValues != null) {
-			tagDefaultAttributeValues = new HashMap<Collection.Key, Map<Collection.Key, Object>>();
+			tagDefaultAttributeValues = new ConcurrentHashMap<Collection.Key, Map<Collection.Key, Object>>();
 			Iterator<Entry<Collection.Key, Map<Collection.Key, Object>>> it = other.tagDefaultAttributeValues.entrySet().iterator();
 			Entry<Collection.Key, Map<Collection.Key, Object>> e;
 			Iterator<Entry<Collection.Key, Object>> iit;
@@ -112,7 +111,7 @@ public abstract class ApplicationContextSupport implements ApplicationContext {
 			while (it.hasNext()) {
 				e = it.next();
 				iit = e.getValue().entrySet().iterator();
-				map = new HashMap<Collection.Key, Object>();
+				map = new ConcurrentHashMap<Collection.Key, Object>();
 				while (iit.hasNext()) {
 					ee = iit.next();
 					map.put(ee.getKey(), ee.getValue());
@@ -189,7 +188,7 @@ public abstract class ApplicationContextSupport implements ApplicationContext {
 
 	@Override
 	public void setTagAttributeDefaultValues(PageContext pc, Struct sct) {
-		if (tagDefaultAttributeValues == null) tagDefaultAttributeValues = new HashMap<Collection.Key, Map<Collection.Key, Object>>();
+		if (tagDefaultAttributeValues == null) tagDefaultAttributeValues = new ConcurrentHashMap<Collection.Key, Map<Collection.Key, Object>>();
 		initTagDefaultAttributeValues(config, tagDefaultAttributeValues, sct);
 	}
 
@@ -238,7 +237,7 @@ public abstract class ApplicationContextSupport implements ApplicationContext {
 
 				if (tag != null) {
 					sct.removeEL(e.getKey());
-					map = new HashMap<Collection.Key, Object>();
+					map = new ConcurrentHashMap<Collection.Key, Object>();
 					iit = attrs.entryIterator();
 					while (iit.hasNext()) {
 						e = iit.next();
@@ -364,7 +363,7 @@ public abstract class ApplicationContextSupport implements ApplicationContext {
 
 	private static Map<String, String> toMap(Struct sct) {
 		Iterator<Entry<Key, Object>> it = sct.entryIterator();
-		Map<String, String> map = new HashMap<String, String>();
+		Map<String, String> map = new ConcurrentHashMap<String, String>();
 		Entry<Key, Object> e;
 		while (it.hasNext()) {
 			e = it.next();
