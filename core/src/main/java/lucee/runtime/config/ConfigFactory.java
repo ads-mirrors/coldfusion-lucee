7dd81e06f0add99308e5692f87844e7c25ba5c84
Zac Spitzer <zac.spitzer@gmail.com>
2025-08-04 11:24:52 +0200

    LDEV-5750 change log level from INFO to ERRRO when CFConfig.json is reset

--- core/src/main/java/lucee/runtime/config/ConfigFactory.java
+++ core/src/main/java/lucee/runtime/config/ConfigFactory.java
@@ -228,7 +228,7 @@ public abstract class ConfigFactory {
 				while ((bugFile = configDir.getRealResource("lucee-" + type + "." + (count++) + ".buggy")).exists()) {
 				}
 
-				LogUtil.log(ThreadLocalPageContext.getConfig(), Log.LEVEL_INFO, ConfigFactory.class.getName(),
+				LogUtil.log(ThreadLocalPageContext.getConfig(), Log.LEVEL_ERROR, ConfigFactory.class.getName(),
 						"The configuration file [" + configFile
 								+ "] contained syntax errors and could not be read. A new configuration file has been created, and the invalid file has been renamed to [" + bugFile
 								+ "].");
