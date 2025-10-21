03861100ce7de6621138d74427948046a3b9cfbe
Michael Offner <michael@lucee.org>
2025-10-21 15:44:56 +0200

    LDEV-5863 - revert

--- core/src/main/java/lucee/runtime/config/ConfigAdmin.java
+++ core/src/main/java/lucee/runtime/config/ConfigAdmin.java
@@ -330,7 +330,7 @@ public final class ConfigAdmin {
 	private synchronized void _store() throws ConverterException, IOException {
 		JSONConverter json = new JSONConverter(true, CharsetUtil.UTF8, JSONDateFormat.PATTERN_CF, false);
 		String str = json.serialize(null, root, SerializationSettings.SERIALIZE_AS_ROW, true);
-		IOUtil.write(config.getConfigFile(), str, CharsetUtil.UTF8, false, true);
+		IOUtil.write(config.getConfigFile(), str, CharsetUtil.UTF8, false);
 	}
 
 	private synchronized void _reload() throws PageException, ClassException, IOException, TagLibException, FunctionLibException, BundleException {
